eba66a547a359cee997cf0d73c5e3823951f6de6
ZianeA <wit_wit90@hotmail.com>
2021-06-06 11:58:38 +0100

    Add a comment

--- app/src/main/java/com/example/continuousintegration/MainActivity.kt
+++ app/src/main/java/com/example/continuousintegration/MainActivity.kt
@@ -7,5 +7,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // This change is not allowed to be pushed directly to the remote master branch.
+        // I have to create a pull request first.
     }
 }
